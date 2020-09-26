8968709c421faaf052848aaed0bef6fc35270330
Mark Woodward <mark@markwoodward.com.au>
2020-09-26 16:01:15 +1000

    add carbon-fields package, homepage slider admin, homepage template

--- public/wp-content/themes/seats-plus-2020/functions.php
+++ public/wp-content/themes/seats-plus-2020/functions.php
@@ -8,3 +8,38 @@
  * Author: Mark Woodward | 360South
  * URL: https://360south.com.au
  */
+
+// Add carbon fields for field csutomisations
+// https://carbonfields.net/docs/
+
+use Carbon_Fields\Container;
+use Carbon_Fields\Field;
+
+add_action('carbon_fields_register_fields', 'crb_attach_theme_options');
+function crb_attach_theme_options()
+{
+    Container::make('post_meta', 'Slider')
+        ->where('post_type', '=', 'page')
+        ->where('post_template', '=', 'template-home-page.php')
+        ->add_fields(array(
+            Field::make('complex', 'crb_slides')->add_fields(array(
+                Field::make('image', 'image')->set_width(2),
+                Field::make('textarea', 'crb_caption', 'Caption')
+                    ->set_rows(4)->set_width(20),
+                Field::make('association', 'crb_association', 'Link to')
+                    ->set_types(
+                        array(
+                            array('type' => 'post', 'post_type' => 'product'),
+                            array('type' => 'post', 'post_type' => 'page')
+                        )
+                    )->set_max(1)->set_width(50),
+            ))->set_layout('tabbed-horizontal'),
+        ));
+}
+
+add_action('after_setup_theme', 'crb_load');
+function crb_load()
+{
+    require_once('vendor/autoload.php');
+    \Carbon_Fields\Carbon_Fields::boot();
+}
---
+++ public/wp-content/themes/seats-plus-2020/template-home-page.php
@@ -0,0 +1,122 @@
+<?php /* Template Name: Home Page Template */ get_header(); ?>
+
+<main role="main">
+	<!-- section -->
+	<section>
+
+		<h1><?php the_title(); ?></h1>
+
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+				<!-- article -->
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+					<?php
+					$slider = carbon_get_the_post_meta('crb_slides');
+
+					if (!empty($slider)) {
+						echo '<ul>';
+						foreach ($slider as $key => $slide) {
+							$caption = $slide['crb_caption'];
+							$link_url = esc_url(get_permalink($slide['crb_association'][0]['id']));
+							$link_title = get_the_title($slide['crb_association'][0]['id']);
+
+							echo '<li>';
+							echo wp_get_attachment_image($slide['image'], [1200, 300]);
+							echo '<div class="content">';
+							echo '<p>' . $caption . '</p>';
+							echo '<a href="' . $link_url . '"';
+							echo 'title="' . $link_title . '">';
+							echo $link_title;
+							echo '</a>';
+							echo '</div>';
+							echo '</li>';
+						}
+						echo '</ul>';
+					} ?>
+
+					<?php the_content(); ?>
+
+					<?php
+					$orderby = 'name';
+					$order = 'asc';
+					$hide_empty = true;
+					$cat_args = array(
+						'orderby'    => $orderby,
+						'order'      => $order,
+						'hide_empty' => $hide_empty,
+					);
+
+					$product_categories = get_terms('product_cat', $cat_args);
+
+					if (!empty($product_categories)) {
+						echo '<ul>';
+						foreach ($product_categories as $key => $category) {
+							echo '<li>';
+							echo '<a href="' . get_term_link($category) . '" >';
+							echo $category->name;
+							echo '</a>';
+							echo '</li>';
+						}
+						echo '</ul>';
+					} ?>
+
+					<?php
+					$about = get_page_by_path('about-us');
+					$link_url = esc_url(get_permalink($about));
+
+					echo '<h2>' . $about->post_title . '</h2>';
+					echo $about->post_content; // get extract instead
+					echo '<a href="' . $link_url . '" title="' . $about->post_title . '">Read More</a>';
+					?>
+
+
+					<?php
+
+					$args = array(
+						'post_type' => 'project',
+						'order'    => 'ASC'
+					);
+
+					$projects = get_posts($args);
+					if (!empty($projects)) {
+						echo '<ul>';
+						foreach ($projects as $key => $project) {
+							$link_url = esc_url(get_permalink($project));
+
+							echo '<li>';
+							echo '<h4>' . $project->post_title . '</h4>';
+							echo '<a href="' . $link_url . '" >View Project</a>';
+							echo '</li>';
+						}
+						echo '</ul>';
+					} ?>
+
+					<br class="clear">
+
+					<?php edit_post_link(); ?>
+
+				</article>
+				<!-- /article -->
+
+			<?php endwhile; ?>
+
+		<?php else : ?>
+
+			<!-- article -->
+			<article>
+
+				<h2><?php _e('Sorry, nothing to display.', 'html5blank'); ?></h2>
+
+			</article>
+			<!-- /article -->
+
+		<?php endif; ?>
+
+	</section>
+	<!-- /section -->
+</main>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
