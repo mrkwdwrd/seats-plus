598ba0155f3c89ffdce75b8aaa8c454221857545
Mark Woodward <markw@digitalbridge.com.au>
2020-09-29 00:37:05 +1000

    woocommerce configs, vuews

--- public/wp-content/themes/seats-plus-2020/functions.php
+++ public/wp-content/themes/seats-plus-2020/functions.php
@@ -308,5 +308,6 @@ add_action('carbon_fields_register_fields', 'crb_attach_theme_options');
 add_action('after_setup_theme', 'crb_load');
 add_action('after_setup_theme', 'add_woocommerce_support');
 
-remove_action('woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
-remove_action('woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+// remove_action('woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+// remove_action('woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+// add_filter('woocommerce_enqueue_styles', '__return_false');
