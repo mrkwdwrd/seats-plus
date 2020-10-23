4e477137f0822b92d8d5b38216327cdeaac04c9b
Mark Woodward <mark@markwoodward.com.au>
2020-10-23 15:08:32 +1100

    basic faq template

---
+++ wp-content/themes/seats-plus-2020/template-faqs.php
@@ -0,0 +1,57 @@
+<?php
+/* Template Name: FAQs Template */
+get_header();
+?>
+
+<main id="colour-chart">
+	<header role="banner">
+		<div class="container">
+			<nav class="breadcrumbs">
+				<?php the_breadcrumb(); ?>
+			</nav>
+			<h2><?php the_title(); ?></h2>
+		</div>
+	</header>
+
+	<section>
+		<div class="container">
+			<header class="row">
+				<div class="col-xs-12">
+					<h1><?php the_title(); ?></h1>
+				</div>
+			</header>
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+					<div class="row">
+						<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> class="col-xs-8">
+							<?php the_content(); ?>
+							<?php edit_post_link(); ?>
+						</article>
+					</div>
+
+					<?php $faqs = carbon_get_the_post_meta('crb_faqs');	?>
+					<ol class="accordion row">
+						<?php foreach ($faqs as $key => $faq) : ?>
+							<li class="col-xs-12">
+								<div class="title">
+									<h3><?php echo $faq['title']; ?></h3>
+								</div>
+								<div class="content">
+									<?php echo apply_filters('the_content', $faq['content']); ?>
+								</div>
+							</li>
+						<?php endforeach; ?>
+					</ol>
+		</div>
+	<?php endwhile; ?>
+
+<?php else : ?>
+	<article>
+		<h2><?php _e('Sorry, nothing to display.'); ?></h2>
+	</article>
+<?php endif; ?>
+</div>
+</div>
+	</section>
+</main>
+
+<?php get_footer(); ?>
