8ccafadd3e5a92928c47edd9628ccca6c3e60441
Mark Woodward <mark@markwoodward.com.au>
2020-12-01 16:12:26 +1100

    google tag scripts

--- wp-content/themes/seats-plus-2020/header.php
+++ wp-content/themes/seats-plus-2020/header.php
@@ -34,6 +34,25 @@
   <!-- Share This  -->
   <script type='text/javascript' src='https://platform-api.sharethis.com/js/sharethis.js#property=5fb0ee183d5b13001279cc15&product=sop' async='async'></script>
 
+  <!-- Google Tag Manager -->
+  <script>
+    (function(w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({
+        'gtm.start': new Date().getTime(),
+        event: 'gtm.js'
+      });
+      var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != 'dataLayer' ? '&l=' + l : '';
+      j.async = true;
+      j.src =
+        'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+      f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-MTMNPX4');
+  </script>
+  <!-- End Google Tag Manager -->
+
   <?php wp_head(); ?>
 </head>
 
